d13c3c8b000fecc849812d9288e39ec8a16fb766
Tushar Tiwari <tutiwari@paypal.com>
2013-07-30 13:26:14 +0530

    Updated Methods

--- src/mainPackage/AmericanAuction.java
+++ src/mainPackage/AmericanAuction.java
@@ -7,48 +7,53 @@ public class AmericanAuction {
 	Integer numofPlayers;
 	ArrayList<Player> players = new ArrayList<Player>();
 	ArrayList<Card> cardsOnAuction = new ArrayList<Card>();
-	Card bidItem;
-	Character[] suit = { 'D', 'H', 'S', 'C'};
+	Card auctionItem;
+	Character[] suit = { 'D', 'H', 'S', 'C' };
 
-	public AmericanAuction(int numofPlayers) {
+	public AmericanAuction(int numofPlayers, char auctionSuite) {
 		this.numofPlayers = numofPlayers;
-		bidItem = null;
+		auctionItem = null;
 
-		// Diamond Cards
+		distributeCards(numofPlayers, auctionSuite);
+	}
+
+	private void distributeCards(int numofPlayers, char auctionSuite) {
+
+		// Auction Cards
 		for (int i = 0; i < 13; i++) {
-			cardsOnAuction.add(new Card(i + 1, 'D'));
+			cardsOnAuction.add(new Card(i + 1, auctionSuite));
 		}
 
 		// Initialise Player with arraylist of cards
 		for (int i = 0; i < numofPlayers; i++) {
 			ArrayList<Card> playerCards = new ArrayList<Card>();
-			for (int j = 0; j < 13; i++) {
+			for (int j = 0; j < 13; j++) {
 				playerCards.add(new Card(j + 1, suit[i + 1]));
 			}
 			players.add(new Player(playerCards));
 		}
 	}
 
-	private Card nextCard() {
+	private Card nextAuctionCard() {
 
 		Random randomGenerator = new Random();
 		int index = randomGenerator.nextInt(cardsOnAuction.size());
-		bidItem = cardsOnAuction.get(index);
+		auctionItem = cardsOnAuction.get(index);
 		cardsOnAuction.remove(index);
-		return bidItem;
+		return auctionItem;
 	}
 
-	public ArrayList<Integer> getPlayerBids() {
-		ArrayList<Integer> playerBids = new ArrayList<Integer>();
+	public ArrayList<Card> PlayerBids() {
+		ArrayList<Card> playerBids = new ArrayList<Card>();
 		for (int i = 0; i < players.size(); i++)
-			playerBids.add(players.get(i).getMyBid().getValue());
+			playerBids.add(players.get(i).getMyBid());
 
 		return playerBids;
 	}
 
-	public ArrayList<Float> getPlayerScores() {
+	public ArrayList<Float> PlayerScores() {
 		ArrayList<Float> playerScores = new ArrayList<Float>();
-		for (int i = 0; i < playerScores.size(); i++)
+		for (int i = 0; i < players.size(); i++)
 			playerScores.add(players.get(i).getScore());
 
 		return playerScores;
@@ -61,35 +66,48 @@ public class AmericanAuction {
 		}
 	}
 
-	public void roundWinner() {
+	public ArrayList<Integer> roundWinner() {
 		int maxBid = players.get(0).getMyBid().getValue();
 		ArrayList<Integer> winnerIndex = new ArrayList<Integer>();
-		// Winner of a round
-		
+		winnerIndex.add(0);
 		for (int i = 1; i < players.size(); i++) {
 			int playerBid = players.get(i).getMyBid().getValue();
 			if (playerBid > maxBid) {
 				winnerIndex.clear();
 				maxBid = playerBid;
 				winnerIndex.add(i);
-			}
-			if (playerBid == maxBid) {
+			} else if (playerBid == maxBid) {
 				winnerIndex.add(i);
 			}
 		}
-		updateScore(winnerIndex);
+		System.out.println("Winner Index: " + winnerIndex);
+		return winnerIndex;
 	}
 
 	public void playRound() {
-		nextCard();
 
-		// Update winner Score
-		roundWinner();
+		nextAuctionCard();
+		for (int i = 0; i < players.size(); i++) {
+			players.get(i).nextBid();
+		}
+		System.out.print(getCardOnAuction() + " --> ");
+		System.out.println(PlayerBids());
+		updateScore(roundWinner());
+		System.out.println("Current scores: " + PlayerScores());
+	}
+
+	public ArrayList<Integer> playGame() {
+
+		for (int j = 0; j < 13; j++) {
+			playRound();
+		}
+		return gameWinner();
 	}
 
 	public ArrayList<Integer> gameWinner() {
 		float maxScore = players.get(0).getScore();
 		ArrayList<Integer> gameWinnerIndex = new ArrayList<Integer>();
+		gameWinnerIndex.add(0);
 		for (int i = 1; i < players.size(); i++) {
 			if (players.get(i).getScore() > maxScore) {
 				maxScore = players.get(i).getScore();
@@ -102,16 +120,15 @@ public class AmericanAuction {
 		return gameWinnerIndex;
 	}
 
-	public ArrayList<Integer> playGame() {
-
-		for (int j = 0; j < 13; j++) {
-			playRound();
-		}
-		return gameWinner();
-	}
-	
 	public Card getCardOnAuction() {
-		return bidItem;
+		return auctionItem;
+	}
+
+	public static void main(String[] args) {
+
+		AmericanAuction game = new AmericanAuction(3, 'D');
+		System.out.println(game.playGame());
+
 	}
 
 }
