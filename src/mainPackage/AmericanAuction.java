a3d4cf43be4dfad6db33670d25fc1b949a4a74d3
Tushar Tiwari <tutiwari@paypal.com>
2013-07-30 12:12:05 +0530

    Added member functions for printing purposes

--- src/mainPackage/AmericanAuction.java
+++ src/mainPackage/AmericanAuction.java
@@ -5,26 +5,36 @@ import java.util.Random;
 
 public class AmericanAuction {
 	Integer numofPlayers;
-	ArrayList<Player> players;
-	ArrayList<Card> cardsOnAuction;
+	ArrayList<Player> players = new ArrayList<Player>();
+	ArrayList<Card> cardsOnAuction = new ArrayList<Card>();
 	Card bidItem;
+	Character[] suit = { 'H', 'S', 'C' };
 
 	public AmericanAuction(int numofPlayers) {
 		this.numofPlayers = numofPlayers;
 		bidItem = null;
-		for (int i = 0; i < numofPlayers; i++) {
-			players.add(new Player());
-		}
+
+		// Diamond Cards
 		for (int i = 0; i < 13; i++) {
 			cardsOnAuction.add(new Card(i + 1, 'D'));
 		}
+
+		// Initialise Player with arraylist of cards
+		for (int i = 0; i < numofPlayers; i++) {
+			ArrayList<Card> playerCards = new ArrayList<Card>();
+			for (int j = 0; j < 13; i++) {
+				playerCards.add(new Card(j + 1, suit[i]));
+			}
+			players.add(new Player(playerCards));
+		}
+
 	}
 
 	public Card getCardOnAuction() {
 		return bidItem;
 	}
 
-	public Card nextCard() {
+	private Card nextCard() {
 
 		Random randomGenerator = new Random();
 		int index = randomGenerator.nextInt(cardsOnAuction.size());
@@ -32,21 +42,37 @@ public class AmericanAuction {
 		cardsOnAuction.remove(index);
 		return bidItem;
 	}
-	
+
 	public ArrayList<Integer> getPlayerBids() {
-		ArrayList<Integer> playerBids;
-		for (int i = 0; i < players.size(); i++) {
-			playerBids.add(players.get(i).getMyBid());
-		}
+		ArrayList<Integer> playerBids = new ArrayList<Integer>();
+		for (int i = 0; i < players.size(); i++)
+			playerBids.add(players.get(i).getMyBid().getValue());
+
 		return playerBids;
 	}
 
-	public void playRound() {
+	public ArrayList<Float> getPlayerScores() {
+		ArrayList<Float> playerScores = new ArrayList<Float>();
+		for (int i = 0; i < playerScores.size(); i++)
+			playerScores.add(players.get(i).getScore());
+
+		return playerScores;
+	}
+
+	private void updateScore(ArrayList<Integer> winnerIndex) {
+		for (int i = 0; i < winnerIndex.size(); i++) {
+			players.get(winnerIndex.get(i)).updateScore(
+					(getCardOnAuction().getValue() / winnerIndex.size()));
+		}
+	}
+
+	public void roundWinner() {
+		int maxBid = players.get(0).getMyBid().getValue();
+		ArrayList<Integer> winnerIndex = new ArrayList<Integer>();
+		// Winner of a round
 		
-		int maxBid = players.get(0).getMyBid();
-		ArrayList<Integer> winnerIndex;
 		for (int i = 1; i < players.size(); i++) {
-			int playerBid = players.getMyBid();
+			int playerBid = players.get(i).getMyBid().getValue();
 			if (playerBid > maxBid) {
 				winnerIndex.clear();
 				maxBid = playerBid;
@@ -56,16 +82,18 @@ public class AmericanAuction {
 				winnerIndex.add(i);
 			}
 		}
-		for (int i = 0; i < winnerIndex.size(); i++) {
-			players.get(winnerIndex.get(i)).updateScore(
-					(getCardOnAuction().getValue() / winnerIndex.size()));
-		}
-		
+	}
+
+	public void playRound() {
+		nextCard();
+
+		// Update winner Score
+		roundWinner();
 	}
 
 	public ArrayList<Integer> gameWinner() {
-		int maxScore = players.get(0).getScore();
-		ArrayList<Integer> gameWinnerIndex = 0;
+		float maxScore = players.get(0).getScore();
+		ArrayList<Integer> gameWinnerIndex = new ArrayList<Integer>();
 		for (int i = 1; i < players.size(); i++) {
 			if (players.get(i).getScore() > maxScore) {
 				maxScore = players.get(i).getScore();
@@ -79,10 +107,7 @@ public class AmericanAuction {
 	}
 
 	public ArrayList<Integer> playGame() {
-		
-		for (int i = 0; i < players.size(); i++)
-			nextCard();
-		
+
 		for (int j = 0; j < 13; j++) {
 			playRound();
 		}
